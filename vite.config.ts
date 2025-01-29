5d05808b32d28b1c36422e924925680846f603ab
Mario Micklisch <mario@favo.org>
2025-01-29 16:16:27 +0100

    Fix twister missing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ['mersenne-twister'],
     exclude: ['lucide-react'],
     esbuildOptions: {
       define: {
