fb3088ea067b683d903bcf91cfbee434283f576e
Mario Micklisch <mario@favo.org>
2025-02-11 11:59:45 +0100

    Fix signal reset

--- src/components/SignalsTable.tsx
+++ src/components/SignalsTable.tsx
@@ -82,7 +82,7 @@ export function SignalsTable({ selectedApp, selectedUser, type = 'signals' }: Si
         "type": "string"
       }
     ],
-    "name": "resetUserSignalsWithReason",
+    "name": "resetUserSignalsByAppAdminWithReason",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
